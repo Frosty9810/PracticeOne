6227b0fa99ee81267e8fa050e56682dd525fa9fa
Frosty9810 <42982241+Frosty9810@users.noreply.github.com>
2020-04-02 16:02:25 -0400

    Changing 2.js(branchTwo)

--- src/2.js
+++ src/2.js
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 /* My File Edition */
 =======
 /***** My new Master File Edition********/
 >>>>>>> Changing 2.js (master)
+=======
+/* My Other Edition */
+>>>>>>> Changing 2.js(branchTwo)
